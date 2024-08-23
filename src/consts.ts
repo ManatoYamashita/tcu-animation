b86aa5cf171c84fe93f770dd370611124b014bd7
yamashita <g2172117@tcu.ac.jp>
2024-08-23 14:12:47 +0900

    [fix]ogp and sitetitle

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog'
-export const SITE_DESCRIPTION = 'Welcome to my website!'
+export const SITE_TITLE = '都市大アニメーション | TCU Animation studio'
+export const SITE_DESCRIPTION = '都市大アニメーション(東京都市大学アニメーションスタジオ)の公式サイトです。都市大の学生有志で構成されています。世田谷区と共同でアニメを製作中！'
